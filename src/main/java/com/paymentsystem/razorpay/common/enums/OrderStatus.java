dc963131659a272ef702c21eb0ca137129fb04e7
Niraj <niraj@MacBook-Pro.local>
2026-06-25 23:57:25 +0530

    Added OrderRecord entity in Payment Module along with Money as a common entity and OrderStatus enum

---
+++ src/main/java/com/paymentsystem/razorpay/common/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.paymentsystem.razorpay.common.enums;
+
+public enum OrderStatus {
+
+    CREATED,
+    ATTEMPTED,
+    PAID,
+    CANCELLED
+}
---
+++ src/main/java/com/paymentsystem/razorpay/payment/entity/OrderRecord.java
@@ -0,0 +1,53 @@
+package com.paymentsystem.razorpay.payment.entity;
+
+import com.paymentsystem.razorpay.common.entity.Money;
+import com.paymentsystem.razorpay.common.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Table(name = "order_record",indexes = {
+        @Index(name = "idx_order_id_merchant_id", columnList = "id, merchant_id"),
+        @Index(name = "idx_order_merchant_id", columnList = "merchant_id"),
+})
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    // no FK - cross-service boundary
+    private UUID merchantId;
+
+    @Embedded
+    private Money amount;
+
+    @Column(length = 100)
+    private String receipt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private OrderStatus orderStatus = OrderStatus.CREATED;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer attemps = 0;
+
+    @JdbcTypeCode((SqlTypes.JSON))
+    @Column(columnDefinition = "jsonb")
+    private Map<String, Object> notes;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+}
