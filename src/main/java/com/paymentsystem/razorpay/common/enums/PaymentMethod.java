73a430af0312b7915fcb53b43fb699fc7072b73f
Niraj <niraj@MacBook-Pro.local>
2026-06-26 17:41:51 +0530

    Added Payment entity along with PaymentStatus and PaymentMethod enums

---
+++ src/main/java/com/paymentsystem/razorpay/common/enums/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.paymentsystem.razorpay.common.enums;
+
+public enum PaymentMethod {
+    CARD,
+    UPI,
+    NETBANKING,
+    WALLET,
+}
---
+++ src/main/java/com/paymentsystem/razorpay/payment/entity/Payment.java
@@ -0,0 +1,95 @@
+package com.paymentsystem.razorpay.payment.entity;
+
+
+import com.paymentsystem.razorpay.common.entity.Money;
+import com.paymentsystem.razorpay.common.enums.PaymentMethod;
+import com.paymentsystem.razorpay.common.enums.PaymentStatus;
+import com.paymentsystem.razorpay.merchant.entity.Merchant;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Table(name = "payment")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "order_id",nullable = false)
+    private OrderRecord order;
+
+    @Column(nullable = false)
+    private UUID merchant_id;
+
+    @Embedded
+    private Money amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,length = 30)
+    private PaymentStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,length = 30)
+    private PaymentMethod method;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "method_details",columnDefinition = "jsonb")
+    private Map<String,Object> methodDetails;
+
+    @Column(length = 100)
+    private String bankReference;
+
+    @Column(length = 100)
+    private String processorReference;
+
+    @Column(length = 255)
+    private String errorDescription;
+
+    private LocalDateTime authorizedAt;
+
+    private LocalDateTime capturedAt;
+
+    private LocalDateTime failedAt;
+
+    private LocalDateTime refundedAt;
+
+    private LocalDateTime settledAt;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
